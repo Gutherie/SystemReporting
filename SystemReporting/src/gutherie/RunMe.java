376ad95b20f483fb365f306ff296316a3bacaac3
Jason R. Baker <jasonbaker@ieee.org>
2015-04-30 15:46:16 -0400

    Initial project setup

---
+++ SystemReporting/src/gutherie/RunMe.java
@@ -0,0 +1,34 @@
+package gutherie;
+
+/*
+ *   This file is part of SystemReporting.
+ *
+ *   SystemReporting is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SystemReporting is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SystemReporting.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class RunMe {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	private static final String INTRO = ""
+			+ "Welcome to the SystemReporting tool." + System.lineSeparator()
+			 + System.lineSeparator() 
+			+ "SystemReporting  Copyright (C) 2015  Jason R. Baker" + System.lineSeparator()
+			+ "This program comes with ABSOLUTELY NO WARRANTY." + System.lineSeparator()
+			+ "This is free software, and you are welcome to redistribute it" + System.lineSeparator()
+			+ "under certain conditions.";
+}
